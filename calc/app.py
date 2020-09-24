4b97a36383db5cfcfc53a9c53b6302ea72eea1d1
Marina8989 <marina.mill209@gmail.com>
2020-09-24 15:21:49 -0700

    flask practice

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,24 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+app = Flask(__name__)
+
+@app.route("/add")
+def add_num():
+    """Add a and b"""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a, b)
+
+    return str(result)
+
+@app.route("/sub")
+def sub_num():
+     """Subtract a and b"""
+
+     a = int(request.args.get("a"))
+     b = int(request.args.get("b"))
+     result = sub(a, b)
+
+     return str(result)  
+
